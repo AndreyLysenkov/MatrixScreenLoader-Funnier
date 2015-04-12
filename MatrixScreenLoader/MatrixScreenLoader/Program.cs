0145681e2b1199099929ebd9a63d4cc381f2b8af
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-04-12 23:48:06 +0300

    Upgradet many things
    I just fogot, I'm doing it with git

--- MatrixScreenLoader/MatrixScreenLoader/Program.cs
+++ MatrixScreenLoader/MatrixScreenLoader/Program.cs
@@ -2,11 +2,69 @@
 using System.Collections.Generic;
 using System.Text;
 
+/*
+ * Downthere complete sucks is happening;
+ * Please, make sure, you took children away from the screen;
+ * Also current code unrecommended to view for person with weak psyhological health;
+ * And remember: I did warn you;
+ * Best wishes,
+ *          Andrey Lysenkov (Allan Awake);
+ */
+
 namespace MatrixScreenLoader
 {
 
     public static class MatrixScreen
     {
+
+        public class Setting
+        {
+
+            const char value_separator = '=';
+
+            private string[][] _parametrs = 
+                {new string[] {"width", "smth"}, {"height", "smth"}, {"timeout", "100"}};
+
+            public Setting()
+            {   }
+
+            public void Set(string[] args)
+            {
+                if (args == null)
+                    return;
+                for (int i = 0; (i < args.Length); i++)
+                {
+                    string parametr = args[i];
+                    if ((parametr == null) || (parametr.Length == 0))
+                        continue;
+                    string[] split_parametrs = parametr.Split(new char[] {value_separator}, 2);
+                    this.SetParametr(name: split_parametrs[0], value: split_parametrs[1]);
+                }
+            }
+
+            public void DEBUG_ShowParametrs(string template = "{0} = {1}")
+            {
+                for (int i = 0; i < _parametrs.Length; i++)
+                {
+                    Console.WriteLine(template, _parametrs[i][0], _parametrs[i][1]);
+                }
+            }
+
+            private bool SetParametr(string name, object value)
+            {
+                bool isFounded = false;
+                for (int i = 0; (i < _parametrs.Length) && (!isFounded); i++)
+                {
+                    if (name.ToLower() == _parametrs[i, 0])
+                    {
+                        _parametrs[i, 1] = value.ToString();
+                        isFounded = true;
+                    }
+                }
+                return isFounded;
+            }
+        
+        }
 
         public static void Run(int width, int height, int timeout)
         {
@@ -37,8 +95,11 @@ namespace MatrixScreenLoader
     {
         static void Main(string[] args)
         {
-
-            MatrixScreen.Run(Console.WindowWidth, Console.WindowHeight, 100);
+            MatrixScreen.Setting current_setting = new MatrixScreen.Setting();
+            current_setting.Set(args);
+            current_setting.DEBUG_ShowParametrs();
+            ///MatrixScreen.Run(Console.WindowWidth, Console.WindowHeight, 100);
+            Console.ReadLine();
         }
     }
 }
